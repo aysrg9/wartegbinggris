b2698ee0e9c466901fa526bde9486e78e0d66554
AYSRG9 <egyditya@gmail.com>
2022-02-06 19:37:03 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.forEach((link) => {
+      link.classList.remove('text-info');
+      link.classList.add('text-white');
+    });
+    link.classList.remove('text-white');
+    link.classList.add('text-info');
+  });
+});
