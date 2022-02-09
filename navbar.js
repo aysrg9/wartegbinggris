1eb29bd003fa41f6efda1dfa4a18d7d12205f5a7
AYSRG9 <egyditya@gmail.com>
2022-02-09 23:04:52 +0700

    update fitur kirim contact

--- navbar.js
+++ navbar.js
@@ -1,12 +1,12 @@
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll(".nav-link");
 
 navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     navLinks.forEach((link) => {
-      link.classList.remove('text-info');
-      link.classList.add('text-white');
+      link.classList.remove("text-info");
+      link.classList.add("text-white");
     });
-    link.classList.remove('text-white');
-    link.classList.add('text-info');
+    link.classList.remove("text-white");
+    link.classList.add("text-info");
   });
 });
