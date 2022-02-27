11fbac4df030763c843cf366895e2ddb2557761c
AYSRG9 <egydttya@gmail.com>
2022-02-27 19:35:27 +0700

    update navbar responsive

---
+++ navbar.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
